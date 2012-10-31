4d376aa5ce4e7c9780535cba2c0f37825a76b709
Steve Tuckner <steve@open-orgsolutions.com>
2012-10-30 20:06:51 -0500

    added in app startup views

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,21 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    user = User.create(:name => "steve")
+    steve = User.create(:name => "steve")
+    abbie = User.create(:name => "abbie")
+    rosie = User.create(:name => "rosie")
+    angie = User.create(:name => "angie")
+
+    empty_dishwasher = Task.create(:name => "empty dishwasher", :user => {:id => abbie.id})
+    rake_leaves = Task.create(:name => "rake leaves", :user => {:id => steve.id})
+    feed_chibi = Task.create(:name => "feed chibi")
+
+    tasksController = TasksController.alloc.init
+    navigationController = UINavigationController.alloc.initWithRootViewController(tasksController)
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = navigationController
+    @window.rootViewController.wantsFullScreenLayout = true
+    @window.makeKeyAndVisible
     true
   end
 end
