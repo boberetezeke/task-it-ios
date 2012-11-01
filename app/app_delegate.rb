1f1f5cbcef0cbe19777078e5bc69d0c4e278483d
Steve Tuckner <stevetuckner@stewdle.com>
2012-11-01 08:45:05 -0500

    removed fake data

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,16 +1,5 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    steve = User.create(:name => "steve")
-    abbie = User.create(:name => "abbie")
-    rosie = User.create(:name => "rosie")
-    angie = User.create(:name => "angie")
-
-    empty_dishwasher = Task.create(:name => "empty dishwasher") #, :user => abbie)
-    empty_dishwasher.user = abbie
-    rake_leaves = Task.create(:name => "rake leaves") #, :user => steve)
-    rake_leaves.user = steve
-    feed_chibi = Task.create(:name => "feed chibi")
-
     tasksController = TasksController.alloc.init
     navigationController = UINavigationController.alloc.initWithRootViewController(tasksController)
 
